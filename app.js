b3d6a52a07a5987f398a0130b11b187c915078a6
MihirSumant <mihirsumant@gmail.com>
2023-08-30 23:44:15 +0530

    Added code for date and delete buttons in new rows

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const inputVendor = document.getElementById('inputVendor');
 const inputPM = document.getElementById('inputPM');
 const inputAmount = document.getElementById('inputAmount');
 
+
 const addExpense = document.getElementById('addToTracker');
 const logNewExpense = document.getElementById('logTableBody');
+
 
 
 addExpense.onclick = () => {
@@ -14,13 +16,26 @@ addExpense.onclick = () => {
     let printPM = inputPM.value;
     let printAmount = inputAmount.value;
     
+    let printTime = new Date();
+    let printRemovebutton = '';
+
     logNewExpense.innerHTML += `
-    <tr>
+    <tr class='newRow'>
     <th scope="col">No.</th>
     <th scope="col">${printName}</th>
     <th scope="col">${printVendor}</th>
     <th scope="col">${printPM}</th>
-    <th scope="col">${printAmount}</th>
-    <th scope="col">Date</th>
-    <th scope="col">Remove</th>`
+    <th scope="col">$${printAmount}</th>
+    <th scope="col">${printTime}</th>
+    <th scope="col"><button class='removeButton'>Delete</button></th>
+    </tr>`
 }
+
+logNewExpense.addEventListener('click', (event) => {
+    if (event.target.classList.contains('removeButton')) {
+        const thisRow = event.target.closest('.newRow');
+        thisRow.remove();
+        console.log('removed');        
+    }
+})
+
